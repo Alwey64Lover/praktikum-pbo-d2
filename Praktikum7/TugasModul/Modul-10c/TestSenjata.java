842e049ec9adb2afd20e8c1404dccb0ae6809064
Alwey <alwey64@gmail.com>
2024-05-02 18:56:42 +0700

    added Praktikum7/TugasModul/Modul-10c

---
+++ Praktikum7/TugasModul/Modul-10c/TestSenjata.java
@@ -0,0 +1,13 @@
+public class TestSenjata{
+    public static void main(String[] args){
+        Senjata ak47 = new Senjata("TAR");
+        Senjata m16 = new Senjata("DOR");
+
+        System.out.print("AK47 menembak 3x: "); ak47.menembak(3);
+        System.out.print("M16 menembak 1x: "); m16.menembak(1);
+
+        System.out.println("AK47 menusuk: " + ak47.menusuk());
+        ak47.pasangBayonet();
+        System.out.println("AK47 menusuk: " + ak47.menusuk());
+    }
+}
